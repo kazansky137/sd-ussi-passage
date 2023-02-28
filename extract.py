d6facd4d340457021d0f0736efd6b635d3e94194
kazansky137 <alain.ninane@me.com>
2023-02-28 18:29:01 +0100

    Refactoring to be pylint compliant

--- extract.py
+++ extract.py
@@ -1,41 +1,46 @@
 #! /usr/bin/env python3
 
-from ldif import LDIFParser
-from pprint import pprint
+"""Extract useful data for 'passage' project from a LDIF file"""
+
 import sys
 import hashlib
+from ldif import LDIFParser
 
-parser = LDIFParser(open(0, "rb"))
+FOURNISSEURS = [1, 2, 3, 8]
 
-for dn, record in parser.parse():
-    dn=str(dn)
-    if dn.startswith("employeenumber=",0,16):
-        uid=record["uid"][0]
-        fgs=record["employeeNumber"][0]
 
-        try:
-            mail=record["mail"][0]
-        except:
-            mail=None
+def _dn_process(dname, record):
+    _dn = str(dname)
+    if _dn.startswith("employeenumber=", 0, 16):
+        uid = record["uid"][0]
+        fgs = record["employeeNumber"][0]
 
-        try:
-            ina=record["UCLInactif"][0]
-        except:
-            ina=None
+        try:  # "mail" attribute must exists
+            mail = record["mail"][0]
+        except KeyError:
+            return
 
-        if mail is not None and ina is None:
+        if "UCLInactif" not in record:
             try:
-                _fours=sorted([int(_f) for _f in record["UCLFournisseur"]])
-                _nfour=0
-                for four in _fours:
-                    if four in [1, 2, 3, 8]:
-                        _nfour = _nfour + 1
-                if _nfour > 0:
-                    hash=hashlib.md5(mail.encode("ascii")).hexdigest()
-                    _upass=record["userPassword"]
-                    for _pass in _upass:
-                        print("u" + hash[4:12], mail, uid, fgs, _pass, _fours)
-            except:
-                pass
+                _upass = record["userPassword"]
+            except KeyError:
+                return
+            _fours = sorted([int(_f) for _f in record["UCLFournisseur"]])
+            _nfour = 0
+            for four in _fours:
+                if four in FOURNISSEURS:
+                    _nfour = _nfour + 1
+            if _nfour > 0:
+                _h = hashlib.md5(mail.encode("ascii")).hexdigest()
+                for _pass in _upass:
+                    print("u" + _h[4:12], mail, uid, fgs, _pass, _fours)
+
+
+if __name__ == "__main__":
+
+    with open(0, "rb") as _fh:
+        parser = LDIFParser(_fh)
+        for _dn, _rec in parser.parse():
+            _dn_process(_dn, _rec)
 
 sys.exit(0)
